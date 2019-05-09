14180b54f47fb9880fd8492339cbcc0d24cd72d2
Pokmand <manudesj@msn.com>
2019-05-09 16:56:47 +0200

    fin de journée Jeudi

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,7 @@
-
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(49.756679, 0.377011),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
